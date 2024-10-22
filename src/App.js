b76a9589c483dabd932395cb7710b6a13c2d13e1
whdjh <whdjh98@naver.com>
2024-10-23 02:07:13 +0900

    Feat: header 구성완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MarketPage from "./pages/MarketPage/MarketPage";
+import Header from "./components/Header"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div>
         <Routes>
           <Route path="items" element={<MarketPage />}/>
